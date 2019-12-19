6b6c862145dd56d35c6c8ed75e5226e3c758612c
bana0731 <kodakgee@gmail.com>
2019-12-19 14:49:46 +0900

    코스피,달러환율 가져오기

---
+++ dollar.py
@@ -0,0 +1,12 @@
+import bs4
+import requests
+
+#환율가져오기
+html = requests.get('https://finance.naver.com/marketindex')
+soup = bs4.BeautifulSoup(html.text,'html.parser')
+dollar = soup.select_one('#exchangeList > li.on > a.head.usd > div > span.value')
+
+print(dollar.text)
+
+#tmi = f'현재 달러 환율은 {dallar.text}입니다.'
+#print(tmi)
--- introduce.py
+++ introduce.py
@@ -1,11 +1,13 @@
 # 명단에서 이름을 뽑아서 영어소개와 한글소개
 import random
 
-name = ['정진영','신동우','이정환']
+name = ['정진영','신동우','이정환','차선우','공찬식']
 eng_name = {
     '정진영':'jinyoung',
     '신동우':'cnu',
-    '이정환':'sandeul'
+    '이정환':'sandeul',
+    '차선우':'baro',
+    '공찬식':'gongchan'
 }
 지목된사람 = random.choice(name)
 지목된영어이름 = eng_name[지목된사람]
@@ -14,6 +16,11 @@ eng_name = {
 intro = '저는 ' + 지목된사람 + '입니다. ' + 'My name is ' + 지목된영어이름
 intro2 = '저는 {}입니다. My name is {}'.format(지목된사람,지목된영어이름)
 intro3 =f'저는 {지목된사람}입니다. My name is {지목된영어이름}'
-print(intro)
-print(intro2)
-print(intro3)
+#print(intro)
+#print(intro2)
+#print(intro3)
+
+year = range(2021,2030)
+comeback = random.choice(year)
+dream = f'안녕하세요. 저는 {지목된사람}{지목된영어이름}입니다. 컴백은 아마도 {comeback}년 입니다.'
+print(dream)
